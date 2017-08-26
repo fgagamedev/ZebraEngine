6b2e8b608e67cf1055e78e9d3d55ffc30dac4f0e
Mariana Pícolo <marianapicolo@hotmail.com>
2017-08-26 16:18:55 -0300

    First application focused on formatting.     - Indentation;     - Conditionals     - Loops

--- src/Customs/QuitButtonScript.cpp
+++ src/Customs/QuitButtonScript.cpp
@@ -1,24 +1,23 @@
 #include "Customs/QuitButtonScript.hpp"
 
-QuitButtonScript::QuitButtonScript(GameObject *owner) : Script(owner) {}
+QuitButtonScript::QuitButtonScript (GameObject *owner) : Script(owner) {}
 
 void QuitButtonScript::Start() {
-  m_uiButton = (UIButton *)GetOwner()->GetComponent("UIButton");
+    m_uiButton = (UIButton *)GetOwner()->GetComponent("UIButton");
 }
 
 void QuitButtonScript::ComponentUpdate() {
-	auto textButton = (UIText *)GetOwner()->GetComponent("UIText");
-  auto soundButton = (UISound *)GetOwner()->GetComponent("UISound");
+    auto textButton = (UIText *)GetOwner()->GetComponent("UIText");
+    auto soundButton = (UISound *)GetOwner()->GetComponent("UISound");
+  
   if (m_uiButton->IsClicked()) {
     soundButton->Play(0, -1);
     SDLSystem::GetInstance()->SetRunning(false);
   }
-  if (m_uiButton->IsOver()) {
-    
-  	textButton->SetColor(160,160,160, 255);
 
-  }else{
-  	
+  if (m_uiButton->IsOver()) {
+    textButton->SetColor(160,160,160, 255);
+  } else {
   	textButton->SetColor(255,255,255, 255);
   }
 }
