c8c08d2406f9c2651a18a731a71a15fa376dcdd4
Mariana Mendes <mare.s.mendes@gmail.com>
2017-08-26 15:53:35 -0300

    Apply some other techniques from stylesheet

--- src/Customs/FirstBossCentralEffectScript.cpp
+++ src/Customs/FirstBossCentralEffectScript.cpp
@@ -1,36 +1,51 @@
 #include "Customs/FirstBossCentralEffectScript.hpp"
 
-FirstBossCentralEffectScript::FirstBossCentralEffectScript(GameObject *owner) : Script(owner) {}
+FirstBossCentralEffectScript::FirstBossCentralEffectScript(GameObject *owner) :
+    Script(owner) {}
 
 void FirstBossCentralEffectScript::Start() {
 
-  CreateAnimations();
-  position = GetOwner()->GetPosition();
-  animator = (Animator *)GetOwner()->GetComponent("Animator");
-  input = InputSystem::GetInstance();
- auto map = SceneManager::GetInstance()->GetScene("Gameplay")->GetGameObject("Map");
-  if(map) GetOwner()->SetZoomProportion(Vector(map->originalWidth/GetOwner()->originalWidth,map->originalHeight/GetOwner()->originalHeight));
+    CreateAnimations();
+    position = GetOwner()->GetPosition();
+    animator = (Animator *)GetOwner()->GetComponent("Animator");
+    input = InputSystem::GetInstance();
+    auto map = SceneManager::GetInstance()->
+                             GetScene("Gameplay")->GetGameObject("Map");
+    if (map) GetOwner()->SetZoomProportion(Vector
+                             (map->originalWidth/GetOwner()->originalWidth,
+                              map->originalHeight/GetOwner()->originalHeight));
 }
-void FirstBossCentralEffectScript::CreateAnimations(){
+void FirstBossCentralEffectScript::CreateAnimations() {
 
-  auto firstBossCentralImage1 = new Image("assets/centroboss11.png",0,0,700, 70);
-  auto firstBossCentralAnimation1= new Animation(GetOwner(),firstBossCentralImage1 );
-
-  for (int i = 0; i < 10; i++)
-    firstBossCentralAnimation1->AddFrame(new Frame(i * 70,0, 70, 70));
-     // animator
-    auto firstBossAnimator = new Animator(GetOwner());
-    firstBossAnimator->AddAnimation("firstBossCentralAnimation1", firstBossCentralAnimation1);
+    auto firstBossCentralImage1 = new Image("assets/centroboss11.png",0,0,
+                                                                      700, 70);
+    auto firstBossCentralAnimation1 = new Animation(GetOwner(),
+                                                   firstBossCentralImage1 );
+    for (int counter = 0; counter < 10; counter++) {
+        firstBossCentralAnimation1->AddFrame(new Frame(counter * 70,0,
+                                                                  70, 70));
+        // animator
+        auto firstBossAnimator = new Animator(GetOwner());
+        firstBossAnimator->AddAnimation("firstBossCentralAnimation1",
+                                                firstBossCentralAnimation1);
+    }
 }
 
 
 void FirstBossCentralEffectScript::ComponentUpdate() {
-  boss = SceneManager::GetInstance()->GetCurrentScene()->GetGameObject("FirstBoss");
-  if(boss) animator->PlayAnimation("firstBossCentralAnimation1");
+    boss = SceneManager::GetInstance()->GetCurrentScene()->
+                                                GetGameObject("FirstBoss");
+    if (boss) {
+        animator->PlayAnimation("firstBossCentralAnimation1");
+    }
 }
 void FirstBossCentralEffectScript::FixedComponentUpdate() {
-  if(boss){
-    position->m_x = boss->GetPosition()->m_x + boss->GetWidth()/2 - GetOwner()->GetWidth()/2 + GetOwner()->GetWidth() / 5.575263158;
-    position->m_y = boss->GetPosition()->m_y + boss->GetHeight()/2 - GetOwner()->GetHeight()/2 +  GetOwner()->GetHeight() / 7.566428571;
+    if (boss) {
+        position->m_x = boss->GetPosition()->m_x + boss->GetWidth() / 2 -
+                                         GetOwner()->GetWidth() / 2 +
+                                         GetOwner()->GetWidth() / 5.575263158;
+        position->m_y = boss->GetPosition()->m_y + boss->GetHeight() / 2 -
+                                        GetOwner()->GetHeight() / 2 +
+                                        GetOwner()->GetHeight() / 7.566428571;
   }
 }
--- src/Customs/FirstBossLifeBorderScript.cpp
+++ src/Customs/FirstBossLifeBorderScript.cpp
@@ -1,50 +1,51 @@
 #include "Customs/FirstBossLifeBorderScript.hpp"
 
-FirstBossLifeBorderScript::FirstBossLifeBorderScript(GameObject *owner) : Script(owner) {}
+FirstBossLifeBorderScript::FirstBossLifeBorderScript(GameObject *owner) :
+    Script(owner) {}
 
 void FirstBossLifeBorderScript::Start() {
 
-  CreateAnimations();
+    CreateAnimations();
 
-  animator = (Animator *)GetOwner()->GetComponent("Animator");
-  input = InputSystem::GetInstance();
-  position = GetOwner()->GetPosition();
-  GetOwner()->SetZoomProportion(Vector(0,0));
-  firstBossAttackCollider = new RectangleCollider(GetOwner(), Vector(0, 0), GetOwner()->GetWidth(), GetOwner()->GetHeight(), 0);
+    animator = (Animator *)GetOwner()->GetComponent("Animator");
+    input = InputSystem::GetInstance();
+    position = GetOwner()->GetPosition();
+    GetOwner()->SetZoomProportion(Vector(0,0));
+    firstBossAttackCollider = new RectangleCollider(GetOwner(), Vector(0, 0),
+                                                  GetOwner()->GetWidth(),
+                                                  GetOwner()->GetHeight(), 0);
 
 }
-void FirstBossLifeBorderScript::CreateAnimations(){
+void FirstBossLifeBorderScript::CreateAnimations() {
 
-  //Image Attacks
-  //332 L por 9 A
-  auto firstBossLifeBorderImage = new Image("assets/Barra_Life_Boss.png",0,0,996, 171);
-  
+    //Image Attacks
+    //332 L por 9 A
+    auto firstBossLifeBorderImage = new Image("assets/Barra_Life_Boss.png",0,0,
+                                                                    996, 171);
 
-  auto firstBossLifeBorderAnimation= new Animation(GetOwner(),firstBossLifeBorderImage );
-  for (int i = 0; i < 19; i++){
-    for(int j =0 ; j< 3 ; j++){
-
-      firstBossLifeBorderAnimation->AddFrame(new Frame(j * 332, i * 9, 332, 9));
+    auto firstBossLifeBorderAnimation = new Animation(GetOwner(),
+                                        firstBossLifeBorderImage );
+    for (int column = 0; column < 19; column++) {
+        for (int row = 0 ; row < 3 ; row++) {
+            firstBossLifeBorderAnimation->AddFrame(new Frame(row * 332,
+                                                       column * 9, 332, 9));
     }
   }
 
 
-
-  //Animator
-  auto firstBossLifeBorderAnimator = new Animator(GetOwner());
-  firstBossLifeBorderAnimator->AddAnimation("firstBossLifeBorderAnimation", firstBossLifeBorderAnimation); 
+    //Animator
+    auto firstBossLifeBorderAnimator = new Animator(GetOwner());
+    firstBossLifeBorderAnimator->AddAnimation("firstBossLifeBorderAnimation",
+                                                firstBossLifeBorderAnimation);
 }
 
 
 void FirstBossLifeBorderScript::ComponentUpdate() {
-    
+
 }
 
 void FirstBossLifeBorderScript::FixedComponentUpdate() {
-
-  animator->PlayAnimation("firstBossLifeBorderAnimation");
-  position->m_x=646;
-  position->m_y=10;
-
+    animator->PlayAnimation("firstBossLifeBorderAnimation");
+    position->m_x = 646;
+    position->m_y = 10;
 }
-
