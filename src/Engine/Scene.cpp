efa035205a50f95814124488a082eeba111c5e95
Diego França <diegobmf@gmail.com>
2017-08-27 13:51:38 -0300

    Fourth application of stylesheet techniques

--- src/Engine/Scene.cpp
+++ src/Engine/Scene.cpp
@@ -7,67 +7,83 @@ Scene::Scene() {}
 Scene::~Scene() {}
 
 void Scene::Start() {
-  for (auto obj : m_gameObjects)
-    obj->Start();
+    for (auto obj : m_gameObjects) {
+        obj->Start();
+    }
 }
 
 void Scene::Update() {
-  std::sort(m_gameObjects.begin(), m_gameObjects.end(), CompareGameObjects);
-  for (auto it : m_gameObjects)
-    if (it->active)
-      it->Update();
+    std::sort(m_gameObjects.begin(), m_gameObjects.end(), CompareGameObjects);
+    for (auto it : m_gameObjects) {
+        if (it->active) {
+            it->Update();
+        }
+    }
 }
 
 void Scene::DrawUpdate() {
-  std::sort(m_gameObjects.begin(), m_gameObjects.end(), CompareGameObjects);
-  for (auto it : m_gameObjects)
-    if (it->active)
-      it->DrawUpdate();
+    std::sort(m_gameObjects.begin(), m_gameObjects.end(), CompareGameObjects);
+    for (auto it : m_gameObjects) {
+        if (it->active) {
+            it->DrawUpdate();
+        }
+    }
 }
 
 void Scene::AddGameObject(GameObject *gameObject) {
-  m_gameObjects.push_back(gameObject);
+    m_gameObjects.push_back(gameObject);
 }
 
 void Scene::AddGameObject(std::vector<GameObject *> gameObjects) {
-  m_gameObjects.insert(std::end(m_gameObjects), std::begin(gameObjects),
-                       std::end(gameObjects));
+    m_gameObjects.insert(std::end(m_gameObjects), std::begin(gameObjects),
+    std::end(gameObjects));
 }
 
 void Scene::FixedUpdate() {
-  for (auto it : m_gameObjects)
-    if (it->active)
-      it->FixedUpdate();
+    for (auto it : m_gameObjects) {
+        if (it->active) {
+            it->FixedUpdate();
+        }
+    }
 }
 
 void Scene::SetState(SceneStates state) {
-  m_currentState = state;
-  INFO("[SCENE] " << m_name << " state: " << m_currentState);
+    m_currentState = state;
+    INFO("[SCENE] " << m_name << " state: " << m_currentState);
 
-  if (state == SCENE_ACTIVATED)
-    Activation();
-  else if (state == SCENE_DEACTIVATED)
-    Deactivation();
-  else if (state == SCENE_HIDDEN)
-    Hidden();
-  else if (state == SCENE_SHOWN)
-    Shown();
+    if (state == SCENE_ACTIVATED) {
+        Activation();
+    } else if (state == SCENE_DEACTIVATED) {
+        Deactivation();
+    } else if (state == SCENE_HIDDEN) {
+        Hidden();
+    } else if (state == SCENE_SHOWN) {
+        Shown();
+    }
 }
 
 void Scene::Activation() {
-  OnActivation();
-  Start();
+    OnActivation();
+    Start();
 }
 
-void Scene::Deactivation() { OnDeactivation(); }
+void Scene::Deactivation() {
+    OnDeactivation();
+}
 
-void Scene::Shown() { OnShown(); }
+void Scene::Shown() {
+    OnShown();
+}
 
-void Scene::Hidden() { OnHidden(); }
+void Scene::Hidden() {
+    OnHidden();
+}
 
 GameObject *Scene::GetGameObject(std::string name) {
-  for (auto it : m_gameObjects)
-    if (it->GetName() == name)
-      return it;
-  return nullptr;
+    for (auto it : m_gameObjects) {
+        if (it->GetName() == name) {
+            return it;
+        }
+    }
+    return nullptr;
 }
--- src/Engine/SceneManager.cpp
+++ src/Engine/SceneManager.cpp
@@ -8,75 +8,87 @@ SceneManager *SceneManager::m_instance = 0;
 
 SceneManager::SceneManager() {}
 
-SceneManager::~SceneManager() { m_instance = nullptr; }
+SceneManager::~SceneManager() {
+    m_instance = nullptr;
+}
 
 SceneManager *SceneManager::GetInstance() {
-  if (!m_instance)
-    m_instance = new SceneManager();
-  return m_instance;
+    if (!m_instance) {
+        m_instance = new SceneManager();
+    }
+    return m_instance;
 }
 
 void SceneManager::SetCurrentScene(std::string sceneName) {
-  auto found = m_scenes.find(sceneName);
-  if (found != m_scenes.end()) {
-    if (m_currentScene.second != nullptr)
-      m_currentScene.second->SetState(SCENE_HIDDEN);
+    auto found = m_scenes.find(sceneName);
+    if (found != m_scenes.end()) {
+        if (m_currentScene.second != nullptr) {
+            m_currentScene.second->SetState(SCENE_HIDDEN);
+        }
 
-    m_currentScene = std::make_pair(found->first, found->second);
-    if (m_currentScene.second->GetState() == SCENE_DEACTIVATED)
-      m_currentScene.second->SetState(SCENE_ACTIVATED);
-    m_currentScene.second->SetState(SCENE_SHOWN);
-  }
+        m_currentScene = std::make_pair(found->first, found->second);
+        if (m_currentScene.second->GetState() == SCENE_DEACTIVATED) {
+            m_currentScene.second->SetState(SCENE_ACTIVATED);
+        }
+        m_currentScene.second->SetState(SCENE_SHOWN);
+    }
 }
 
 std::string SceneManager::GetCurrentSceneName() {
-  /*
-  auto found = m_scenes.find(sceneName);
-  if (found != m_scenes.end()) {
-    if (m_currentScene.second != nullptr)
-      m_currentScene.second->SetState(SCENE_HIDDEN);
+    /*
+    auto found = m_scenes.find(sceneName);
+    if (found != m_scenes.end()) {
+        if (m_currentScene.second != nullptr) {
+            m_currentScene.second->SetState(SCENE_HIDDEN);
+        }
 
-    m_currentScene = std::make_pair(found->first, found->second);
-    if (m_currentScene.second->GetState() == SCENE_DEACTIVATED)
-      m_currentScene.second->SetState(SCENE_ACTIVATED);
-    m_currentScene.second->SetState(SCENE_SHOWN);
-  }
-  */
-  return m_currentScene.second->GetName();
+        m_currentScene = std::make_pair(found->first, found->second);
+        if (m_currentScene.second->GetState() == SCENE_DEACTIVATED) {
+            m_currentScene.second->SetState(SCENE_ACTIVATED);
+        }
+        m_currentScene.second->SetState(SCENE_SHOWN);
+    }
+    */
+    return m_currentScene.second->GetName();
 }
 
 void SceneManager::AddScene(std::pair<std::string, Scene *> scenePair) {
-  scenePair.second->SetName(scenePair.first);
-  m_scenes.insert(scenePair);
+    scenePair.second->SetName(scenePair.first);
+    m_scenes.insert(scenePair);
 }
 
 void SceneManager::Start() {
-  for (auto scene : m_scenes) {
-    if (scene.second->GetState() == SCENE_DEACTIVATED)
-      scene.second->SetState(SCENE_ACTIVATED);
-  }
+    for (auto scene : m_scenes) {
+        if (scene.second->GetState() == SCENE_DEACTIVATED) {
+            scene.second->SetState(SCENE_ACTIVATED);
+        }
+    }
 }
 
 void SceneManager::Update() {
-  if (m_currentScene.second)
-    m_currentScene.second->Update();
+    if (m_currentScene.second) {
+        m_currentScene.second->Update();
+    }
 }
 
 void SceneManager::DrawUpdate() {
-  if (m_currentScene.second)
-    m_currentScene.second->DrawUpdate();
+    if (m_currentScene.second) {
+        m_currentScene.second->DrawUpdate();
+    }
 }
 
 void SceneManager::FixedUpdate() {
-  if (m_currentScene.second)
-    m_currentScene.second->FixedUpdate();
+    if (m_currentScene.second) {
+        m_currentScene.second->FixedUpdate();
+    }
 }
 
 Scene *SceneManager::GetScene(std::string name) {
-  for (auto pair : m_scenes) {
-    if (pair.first == name)
-      return pair.second;
-  }
-  ERROR("Looking for " << name << " scene which does not exist");
-  return nullptr;
+    for (auto pair : m_scenes) {
+        if (pair.first == name) {
+            return pair.second;
+        }
+    }
+    ERROR("Looking for " << name << " scene which does not exist");
+    return nullptr;
 }
