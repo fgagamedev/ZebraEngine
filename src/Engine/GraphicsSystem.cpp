598799bb6bd678a9c0fe9c103ee78cc011df6a5d
Mariana Pícolo <marianapicolo@hotmail.com>
2017-08-27 15:49:10 -0300

    Third application focused on formatting.

--- src/Engine/GraphicsSystem.cpp
+++ src/Engine/GraphicsSystem.cpp
@@ -13,128 +13,140 @@ GraphicsSystem::GraphicsSystem() {}
 GraphicsSystem::~GraphicsSystem() { m_instance = nullptr; }
 
 GraphicsSystem *GraphicsSystem::GetInstance() {
-  if (!m_instance)
-    m_instance = new GraphicsSystem();
-  return m_instance;
+    if (!m_instance) {
+        m_instance = new GraphicsSystem();
+    }
+    return m_instance;
 }
 
 void GraphicsSystem::Draw(Image *img, Vector *position,
                           std::pair<int, int> sizes) {
-  SDL_Rect dest;
-  dest.w = sizes.first;
-  dest.h = sizes.second;
-  dest.x = position->m_x;
-  dest.y = position->m_y;
+    SDL_Rect dest;
+    dest.w = sizes.first;
+    dest.h = sizes.second;
+    dest.x = position->m_x;
+    dest.y = position->m_y;
 
-  // SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), 0, 255, 0,
-  //                        255);
-  // SDL_RenderDrawRect(SDLSystem::GetInstance()->GetRenderer(), &dest);
-  // SDL_RenderDrawLine(SDLSystem::GetInstance()->GetRenderer(), 0, 0, dest.x,
-  //                    dest.y);
+  /* SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), 0, 255, 0, 255);
+     SDL_RenderDrawRect(SDLSystem::GetInstance()->GetRenderer(), &dest);
+     SDL_RenderDrawLine(SDLSystem::GetInstance()->GetRenderer(), 0, 0, dest.x,dest.y);        
+  */
 
-  int result;
-  result = SDL_RenderCopyEx(SDLSystem::GetInstance()->GetRenderer(),
+    int result;
+    result = SDL_RenderCopyEx(SDLSystem::GetInstance()->GetRenderer(),
                             img->GetTexture(), img->GetRect(), &dest,
                             img->GetRotationAngle(), img->GetSDLPivot(),
                             img->GetSDLFlip());
 
-  if (result < 0)
-    ERROR(SDL_GetError());
+    if (result < 0) {
+        ERROR(SDL_GetError());
+    }
 }
 
 void GraphicsSystem::DrawFrame(Image *img, Frame *frame, Vector *position,int destw,int desth) {
-  SDL_Rect dest;
-  dest.w = destw;
-  dest.h = desth;
-  dest.x = position->m_x;
-  dest.y = position->m_y;
+    SDL_Rect dest;
+    dest.w = destw;
+    dest.h = desth;
+    dest.x = position->m_x;
+    dest.y = position->m_y;
 
-  // SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), 0, 255, 0,
-  //                        255);
-  // SDL_RenderDrawRect(SDLSystem::GetInstance()->GetRenderer(), &dest);
-  // SDL_RenderDrawLine(SDLSystem::GetInstance()->GetRenderer(), 0, 0, dest.x,
-  //                    dest.y);
+  /* SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), 0, 255, 0,255);
+     SDL_RenderDrawRect(SDLSystem::GetInstance()->GetRenderer(), &dest);
+     SDL_RenderDrawLine(SDLSystem::GetInstance()->GetRenderer(), 0, 0, dest.x,dest.y);
+  */
 
-  int result;
-  result = SDL_RenderCopyEx(SDLSystem::GetInstance()->GetRenderer(),
+    int result;
+    result = SDL_RenderCopyEx(SDLSystem::GetInstance()->GetRenderer(),
                             img->GetTexture(), frame->GetRect(), &dest,
                             img->GetRotationAngle(), img->GetSDLPivot(),
                             img->GetSDLFlip());
 
-  if (result < 0)
-    ERROR(SDL_GetError());
+    if (result < 0) {
+        ERROR(SDL_GetError());
+    }
 }
 
 void GraphicsSystem::DrawText(SDL_Texture *texture, SDL_Rect *destRect) {
-  SDL_RenderCopy(SDLSystem::GetInstance()->GetRenderer(), texture, NULL,
-                 destRect);
+    SDL_RenderCopy(SDLSystem::GetInstance()->GetRenderer(), texture, NULL, destRect);
 }
 
 void GraphicsSystem::DrawPoint(Vector point) {
-  int result = SDL_RenderDrawPoint(SDLSystem::GetInstance()->GetRenderer(),
-                                   point.m_x, point.m_y);
-  if (result) {
+    int result = SDL_RenderDrawPoint(SDLSystem::GetInstance()->GetRenderer(), 
+    point.m_x, point.m_y);
+
+    if (result) {
     SDL_ERROR("Drawing point error");
   }
 }
 
 void GraphicsSystem::DrawCircle(Vector &center, float radius, Uint8 r, Uint8 g,
                                 Uint8 b, Uint8 a) {
-  SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), r, g, b, a);
-  Vector point;
-  for (int angle = 0; angle < 360; angle++) {
-    point.m_x = center.m_x + cos(angle) * radius;
-    point.m_y = center.m_y - sin(angle) * radius;
-    int result = SDL_RenderDrawPoint(SDLSystem::GetInstance()->GetRenderer(),
+
+    SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), r, g, b, a);
+    Vector point;
+
+    for (int angle = 0; angle < 360; angle++) {
+        point.m_x = center.m_x + cos(angle) * radius;
+        point.m_y = center.m_y - sin(angle) * radius;
+        int result = SDL_RenderDrawPoint(SDLSystem::GetInstance()->GetRenderer(),
                                      point.m_x, point.m_y);
-    if (result < 0)
-      ERROR(SDL_GetError());
+    if (result < 0) {
+        ERROR(SDL_GetError());
+    }
   }
 }
 
 void GraphicsSystem::DrawFillCircle(Vector &center, float radius, Uint8 r,
                                     Uint8 g, Uint8 b, Uint8 a) {
-  SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), r, g, b, a);
-  Vector point;
-  for (float angle = 0; angle < 360; angle += 0.01) {
-    point.m_x = center.m_x + cos(angle) * radius;
-    point.m_y = center.m_y - sin(angle) * radius;
-    int result =
-        SDL_RenderDrawLine(SDLSystem::GetInstance()->GetRenderer(), center.m_x,
-                           center.m_y, point.m_x, point.m_y);
-    if (result < 0)
-      ERROR(SDL_GetError());
+
+    SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), r, g, b, a);
+    Vector point;
+
+    for (float angle = 0; angle < 360; angle += 0.01) {
+        point.m_x = center.m_x + cos(angle) * radius;
+        point.m_y = center.m_y - sin(angle) * radius;
+
+        int result = SDL_RenderDrawLine(SDLSystem::GetInstance()->GetRenderer(),
+        center.m_x, center.m_y, point.m_x, point.m_y);
+
+    if (result < 0) {
+        ERROR(SDL_GetError());
+    }
   }
 }
 
 void GraphicsSystem::DrawFillRectangle(Vector &position, int width, int height,
                                        Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
-  SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), r, g, b, a);
+  
+    SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), r, g, b, a);
 
-  SDL_Rect rect;
-  rect.x = position.m_x;
-  rect.y = position.m_y;
-  rect.w = width;
-  rect.h = height;
+    SDL_Rect rect;
+    rect.x = position.m_x;
+    rect.y = position.m_y;
+    rect.w = width;
+    rect.h = height;
 
-  int result =
-      SDL_RenderFillRect(SDLSystem::GetInstance()->GetRenderer(), &rect);
-  if (result < 0)
-    ERROR(SDL_GetError());
+    int result = SDL_RenderFillRect(SDLSystem::GetInstance()->GetRenderer(), &rect);
+
+  if (result < 0) {
+      ERROR(SDL_GetError());
+  }
 }
 
 void GraphicsSystem::DrawFillRectangle(SDL_Rect* source, int width, int height,
                                        Uint8 r, Uint8 g, Uint8 b, Uint8 a) {
-  SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), r, g, b, a);
 
-  SDL_Rect rect;
-  rect.x = source->x;
-  rect.y = source->y;
-  rect.w = source->w;
-  rect.h = source->h;
+    SDL_SetRenderDrawColor(SDLSystem::GetInstance()->GetRenderer(), r, g, b, a);
 
-  int result =
-      SDL_RenderFillRect(SDLSystem::GetInstance()->GetRenderer(), &rect);
-  if (result < 0)
-    ERROR(SDL_GetError());
+    SDL_Rect rect;
+    rect.x = source->x;
+    rect.y = source->y;
+    rect.w = source->w;
+    rect.h = source->h;
+
+    int result = SDL_RenderFillRect(SDLSystem::GetInstance()->GetRenderer(), &rect);
+
+    if (result < 0) {
+        ERROR(SDL_GetError());
+    }
 }
--- src/Math/Vector.cpp
+++ src/Math/Vector.cpp
@@ -1,69 +1,70 @@
 #include "Math/Vector.hpp"
 
 Vector Vector::operator+(const Vector &vec) {
-  return Vector(m_x + vec.m_x, m_y + vec.m_y);
+    return Vector(m_x + vec.m_x, m_y + vec.m_y);
 }
 
 Vector Vector::operator+=(const Vector &vec) {
-  return Vector(m_x + vec.m_x, m_y + vec.m_y);
+    return Vector(m_x + vec.m_x, m_y + vec.m_y);
 }
 
 Vector Vector::operator-(const Vector &vec) {
-  return Vector(m_x - vec.m_x, m_y - vec.m_y);
+    return Vector(m_x - vec.m_x, m_y - vec.m_y);
 }
 
 Vector Vector::operator-=(const Vector &vec) {
-  return Vector(m_x - vec.m_x, m_y - vec.m_y);
+    return Vector(m_x - vec.m_x, m_y - vec.m_y);
 }
 
 Vector Vector::operator*(float scalar) {
-  return Vector(m_x * scalar, m_y * scalar);
+    return Vector(m_x * scalar, m_y * scalar);
 }
 
 Vector Vector::operator/(float scalar) {
-  return Vector(m_x / scalar, m_y / scalar);
+    return Vector(m_x / scalar, m_y / scalar);
 }
 
 float Vector::GetLength() { return sqrt(m_x * m_x + m_y * m_y); }
 
 float Vector::GetLength(Vector &vec) {
-  return sqrt((vec.m_x * vec.m_x) + (vec.m_y * vec.m_y));
+    return sqrt((vec.m_x * vec.m_x) + (vec.m_y * vec.m_y));
 }
 
 Vector Vector::GetNormalized() {
-  float l = GetLength();
-  if (l > 0) {
-    Vector normalized = (*this) * (1 / l);
-    return normalized;
+    float l = GetLength();
+    if (l > 0) {
+        Vector normalized = (*this) * (1 / l);
+            return normalized;
   }
-  return Vector(0, 0);
+        return Vector(0, 0);
 }
 
 float Vector::GetAngleDegrees(Vector &vec){
 float angle  = atan((vec.m_y - m_y)/(vec.m_x - m_x))*180/3.14;
 
   //1 quadrante
-  if(vec.m_x >=m_x && vec.m_y <= m_y )
-  angle = angle;
-   //2 quadrante
-  if(vec.m_x<m_x && vec.m_y> m_y)
-  angle = 180 + angle;
+    if(vec.m_x >=m_x && vec.m_y <= m_y )
+        angle = angle;
+  //2 quadrante
+    if(vec.m_x<m_x && vec.m_y> m_y)
+        angle = 180 + angle;
   //3 quadrante
-  if(vec.m_x<m_x && vec.m_y< m_y )
-   angle = 180  + angle;
+    if(vec.m_x<m_x && vec.m_y< m_y )
+       angle = 180  + angle;
   //4 quadrante
-  if(vec.m_x >=m_x && vec.m_y <= m_y )
-  angle =360 + angle;
+    if(vec.m_x >=m_x && vec.m_y <= m_y )
+       angle =360 + angle;
 
 return angle;
 }
+
 float Vector::GetAngleRadians(Vector &vec){
 
-return GetAngleDegrees(vec)*3.14/180;
+    return GetAngleDegrees(vec)*3.14/180;
 
 }
 
 float Vector::GetDistance(Vector vec) {
-  Vector subtraction = vec - *this;
-  return GetLength(subtraction);
+    Vector subtraction = vec - *this;
+        return GetLength(subtraction);
 }
