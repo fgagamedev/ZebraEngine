6bf8c11e4e497784581af39762b47c6df56b515f
Mariana Pícolo <marianapicolo@hotmail.com>
2017-09-08 17:45:46 -0300

    Third application - GameController .hpp & .cpp     - Comments in the file of class GameController;     - Comments in the methods, its params and returns;     - Adjusting formatting on files: identation, loops and conditionals.

--- include/Engine/InputSystem.hpp
+++ include/Engine/InputSystem.hpp
@@ -9,13 +9,23 @@
 #include <utility>
 #include <vector>
 
+/*
+    @file InputSystem.hpp
+    @brief This class defines the actions of the buttons needed for playing the game.
+    @copyright MIT License.
+*/
+
+
 class InputSystem {
 public:
-  // singleton getter
+
+  // Singleton getter
   static InputSystem *GetInstance();
-  // update m_states and m_oldStates every frame
+
+  // Update m_states and m_oldStates every frame
   void UpdateStates();
-  // event handling
+
+  // Event handling
   bool GetKeyDown(KeyboardInputGlobal key);
   bool GetKeyUp(KeyboardInputGlobal key);
   bool GetKeyPressed(KeyboardInputGlobal key);
@@ -26,24 +36,30 @@ public:
   GameController *GetGameController(int index);
 
 private:
-  // constructor and destructor
+
+  // Constructor and destructor
   InputSystem();
   ~InputSystem();
+
   // Joystick handling
   void LoadGameControllers();
   void UpdateGameControllers();
   void CheckGameControllersConnections();
-  // private attributes
+
+  // Private attributes
   static InputSystem *m_instance;
-  // keyboard states
+
+  // Keyboard states
   const Uint8 *m_states;
   Uint8 *m_oldStates;
   int m_statesSize;
-  // mouse states
+
+  // Mouse states
   Uint32 m_mouseStates = 0;
   Uint32 m_oldMouseStates = 0;
   int m_mouseX, m_mouseY;
-  // game controllers
+
+  // Game controllers
   std::vector<GameController *> m_gameControllers;
 };
 
--- src/Engine/GameController.cpp
+++ src/Engine/GameController.cpp
@@ -2,47 +2,86 @@
 
 #include "Log/log.hpp"
 
+/*
+    @file GameController.cpp
+    @brief Methods that initializes all buttons actions from game controllers .
+    @copyright MIT License.
+*/
+
+
+/*
+    @brief Initializes GameController instance, to use game controllers.
+    @param[in] gc - game controller inputs.
+    @return void
+*/
 GameController::GameController(SDL_GameController *gc) {
-  if (gc) {
-    INFO("    " << SDL_GameControllerName(gc) << " opened");
-    m_gameController = gc;
+    if (gc) {
+        INFO("    " << SDL_GameControllerName(gc) << " opened");
+        m_gameController = gc;
   } else {
     ERROR("    Could not open game controller " << SDL_GameControllerName(gc));
   }
 
-  m_oldButtonsStates.resize(GC_INPUT_MAX, 0);
-  m_currentButtonsStates.resize(GC_INPUT_MAX, 0);
+    m_oldButtonsStates.resize(GC_INPUT_MAX, 0);
+    m_currentButtonsStates.resize(GC_INPUT_MAX, 0);
 }
 
 GameController::~GameController() { m_gameController = nullptr; }
 
-void GameController::Update() {
-  m_oldButtonsStates = m_currentButtonsStates;
 
-  for (int i = 0; i < GC_INPUT_MAX; i++) {
-    m_currentButtonsStates[i] = SDL_GameControllerGetButton(
-        m_gameController, (SDL_GameControllerButton)i);
+/*
+    @brief Updates the state of a button in a game controller.
+    @return void.
+*/
+void GameController::Update() {
+    m_oldButtonsStates = m_currentButtonsStates;
+
+    for (int i = 0; i < GC_INPUT_MAX; i++) {
+        m_currentButtonsStates[i] = SDL_GameControllerGetButton(m_gameController,
+                                                                (SDL_GameControllerButton)i);
   }
 }
 
+/*
+    @brief Get the current state of a button down on a game controller.
+    @param[in] button - status of the button down.
+    @return bool.
+*/
 bool GameController::GetButtonDown(GameControllerButton button) {
-  if (m_currentButtonsStates[button] && !m_oldButtonsStates[button])
-    return true;
-  return false;
+    if (m_currentButtonsStates[button] && !m_oldButtonsStates[button]){
+        return true;
+    }
+    return false;
 }
 
+/*
+    @brief Get the current state of a button up on a game controller.
+    @param[in] button - status of the button up.
+    @return bool.
+*/
 bool GameController::GetButtonUp(GameControllerButton button) {
-  if (!m_currentButtonsStates[button] && m_oldButtonsStates[button])
-    return true;
-  return false;
+    if (!m_currentButtonsStates[button] && m_oldButtonsStates[button]){
+        return true;
+    }
+    return false;
 }
 
+/*
+    @brief Get the current button pressed of a button on a game controller.
+    @param[in] button - status of a button of the game controller.
+    @return bool.
+*/
 bool GameController::GetButtonPressed(GameControllerButton button) {
-  return SDL_GameControllerGetButton(m_gameController,
+    return SDL_GameControllerGetButton(m_gameController,
                                      (SDL_GameControllerButton)button);
 }
 
+/*
+    @brief Get the current state of an axis control on a game controller.
+    @param[in] axis - current axis of a controller.
+    @return int.
+*/
 int GameController::GetAxis(GameControllerAxis axis) {
-  return SDL_GameControllerGetAxis(m_gameController,
+    return SDL_GameControllerGetAxis(m_gameController,
                                    (SDL_GameControllerAxis)axis);
 }
