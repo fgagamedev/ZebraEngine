4d1704c075c089da5a22eb26e2ececef9fb8626d
amandabezerra <amandabs_contato@hotmail.com>
2017-09-11 21:48:10 -0300

    First application of comments techniques.

--- src/Components/Renderer.cpp
+++ src/Components/Renderer.cpp
@@ -7,37 +7,64 @@
 
 #include <math.h>
 
+/**
+    @file Renderer.cpp
+    @brief Manages renderings of images on the screen.
+    @copyright LGPL. MIT License.
+*/
+
+/**
+    @brief Update the images informations on the screen.
+*/
 void Renderer::ComponentUpdate() {
-    std::pair<int, int> sizes =
+    std::pair<int, int> sizes = 
         std::make_pair(GetOwner()->GetWidth(), GetOwner()->GetHeight());
-     // if(!activation)
-    GraphicsSystem::GetInstance()->Draw(m_image, m_position, sizes);
+        GraphicsSystem::GetInstance()->Draw(m_image, m_position, sizes);
 }
 
+/**
+    @brief  Constructor for the class Renderer.
+    @param[in] owner - Owns the component.
+    @param[in] img
+*/
 Renderer::Renderer(GameObject *owner, Image *img) : Component(owner, C_DRAW) {
     m_image = img;
 
     m_position = GetOwner()->GetPosition();
 
     if (!m_image) {
-      ERROR("Null image on renderer");
+        ERROR("Null image on renderer");
     }
 }
 
+/**
+    @brief Destructor for the class Renderer.
+*/
 Renderer::~Renderer() { delete m_image; }
 
+/**
+    @brief Initializes the renderer.
+*/
 void Renderer::Start() {}
 
+/**
+    @brief Rotate the image towards the calculated angle.
+    @param[in] point
+*/
 void Renderer::RotateTowards(Vector *point) {
     double angles = 0;
     angles = atan2(point->m_y - m_position->m_y, point->m_x - m_position->m_x);
     angles = angles * 180 / 3.1415;
     if (angles < 0) {
-    angles = 360 - (-angles);
+        angles = 360 - (-angles);
     }
     Rotate(angles);
 }
 
+/**
+    @brief Rotate the image.
+    @param[in] angles - Angles of the image.
+*/
 void Renderer::Rotate(double angles) {
     GetImage()->Rotate(angles);
 }
--- src/Engine/Frame.cpp
+++ src/Engine/Frame.cpp
@@ -1,5 +1,18 @@
 #include "Engine/Frame.hpp"
 
+/**
+    @file Frame.cpp
+    @brief Manages the frame of the game.
+    @copyright LGPL. MIT License.
+*/
+
+/**
+    @brief Set start coordinates and frame size.
+    @param[in] x - Horizontal coordinate of the frame.
+    @param[in] y - Vertical coordinate of the frame.
+    @param[in] width - Horizontal frame size
+    @param[in] height - Vertical frame size.
+*/
 Frame::Frame(int x, int y, int width, int height) {
   srcRect.x = x;
   srcRect.y = y;
@@ -7,4 +20,8 @@ Frame::Frame(int x, int y, int width, int height) {
   srcRect.h = height;
 }
 
+/**
+    @brief Get informations about the frame.
+    @return - The rectangular area of the frame
+*/
 SDL_Rect *Frame::GetRect() { return &srcRect; }
