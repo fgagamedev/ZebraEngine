63d558e4c0d78e2026ac87007d12d4ce4a9feefe
alvesisaque <alvesisaque838.ia@gmail.com>
2017-09-13 22:19:24 -0300

    Apply tecniques of comments in class CentralLightScript4

--- include/Customs/CentralLightScript4.hpp
+++ include/Customs/CentralLightScript4.hpp
@@ -1,6 +1,10 @@
+/**
+    @file CentralLightScript4.hpp
+    @brief This class contains all attributes and methods that manages the central light in the game.
+    @copyright LGPL. MIT License.
+*/
 #ifndef __CENTRAL_LIGHT_SCRIPT4__
 #define __CENTRAL_LIGHT_SCRIPT4__
-
 
 #include "Engine/Timer.hpp"
 #include "Engine/GameController.hpp"
@@ -10,30 +14,37 @@
 
 #include "Components/Script.hpp"
 #include "Components/Animator.hpp"
+
 #include "Math/Vector.hpp"
 
 #include <string>
 
 class CentralLightScript4 : public Script {
 
-public:
-  CentralLightScript4(GameObject *owner);
-  std::string GetComponentName() override { return "CentralLightScript4"; };
-  void FixedComponentUpdate() override;
-  void Start() override;
-  void Activate(){active = true;}
+	public:
+		CentralLightScript4(GameObject *owner);
+		std::string GetComponentName() override {
+			return "CentralLightScript4";
+		};
 
-protected:
-  void ComponentUpdate() override;
-private:
-  bool active=false;
-  Timer time;
-  void CreateAnimations();
-  InputSystem *input = nullptr;
-   GameController* gamecontroller = nullptr;
-  Animator *animator = nullptr;
-  Vector *position = nullptr;
-  int play=0;
+		void FixedComponentUpdate() override;
+		void Start() override;
+		void Activate(){active = true;}
+
+	protected:
+		void ComponentUpdate() override;
+
+	private:
+		bool active = false;
+		Timer time;
+
+		void CreateAnimations();
+
+		InputSystem *input = nullptr;
+		 GameController* gamecontroller = nullptr;
+		Animator *animator = nullptr;
+		Vector *position = nullptr;
+		int play = 0;
 
 };
 #endif
--- src/Customs/CentralLightScript4.cpp
+++ src/Customs/CentralLightScript4.cpp
@@ -1,37 +1,66 @@
+/**
+    @file CentralLightScript4.cpp
+    @brief This class contains all attributes and methods that manages the central light in the game.
+    @copyright LGPL. MIT License.
+*/
+
 #include "Customs/CentralLightScript4.hpp"
 #include "Globals/EngineGlobals.hpp"
 
-CentralLightScript4::CentralLightScript4(GameObject *owner) : Script(owner) {}
+/**
+    @brief Initializes CentralLightScript instance.
+    @param[in] GameObject *owner - owns the component.
+*/
+CentralLightScript4::CentralLightScript4(GameObject *owner) : Script(owner) {
+
+}
+
+/**
+    @brief Creates the animations in-game, with some of the gameobject
+    properties.
+*/
 void CentralLightScript4::Start() {
 
-  CreateAnimations();
-  position = GetOwner()->GetPosition();
-  animator = (Animator *)GetOwner()->GetComponent("Animator");
-  input = InputSystem::GetInstance();
-  gamecontroller = input->GetGameController(0);
-  GetOwner()->SetZoomProportion(Vector(0,0));
+    CreateAnimations();
+    position = GetOwner()->GetPosition();
+    animator = (Animator *)GetOwner()->GetComponent("Animator");
+    input = InputSystem::GetInstance();
+    gamecontroller = input->GetGameController(0);
+    GetOwner()->SetZoomProportion(Vector(0,0));
     auto map = SceneManager::GetInstance()->GetScene("Gameplay")->GetGameObject("Map");
-    if(map) GetOwner()->SetZoomProportion(Vector(map->originalWidth/GetOwner()->originalWidth,map->originalHeight/GetOwner()->originalHeight));
+    if(map){
+      GetOwner()->SetZoomProportion(Vector(map->originalWidth/GetOwner()->originalWidth,
+                                           map->originalHeight/GetOwner()->originalHeight));
+    }
 
 }
+
+/**
+    @brief Positions the animations in-game, through the settings of frames,
+    and image's position.
+*/
 void CentralLightScript4::CreateAnimations(){
 
-  auto centrallightSprite = new Image("assets/centro2.png", 0, 0,832, 64);
-  auto centrallightAnimation = new Animation(GetOwner(), centrallightSprite);
-  centrallightAnimation->AddFrame(new Frame(0, 0, 64, 64));
+    auto centrallightSprite = new Image("assets/centro2.png", 0, 0,832, 64);
+    auto centrallightAnimation = new Animation(GetOwner(), centrallightSprite);
+    centrallightAnimation->AddFrame(new Frame(0, 0, 64, 64));
 
-  auto centrallightAnimator = new Animator(GetOwner());
-  centrallightAnimation->SetFramesPerSecond(9);
-  centrallightAnimator->AddAnimation("CENTRAL LIGHT ANIMATION", centrallightAnimation);
+    auto centrallightAnimator = new Animator(GetOwner());
+    centrallightAnimation->SetFramesPerSecond(9);
+    centrallightAnimator->AddAnimation("CENTRAL LIGHT ANIMATION",
+                                     centrallightAnimation);
 
 
 }
 
-
+/**
+    @brief Updates the animator component by looking for a previous
+    light animation.
+*/
 void CentralLightScript4::ComponentUpdate() {
 
-if(!animator->IsPlaying("CENTRAL LIGHT ANIMATION") && active)
-animator->PlayAnimation("CENTRAL LIGHT ANIMATION");
+    if(!animator->IsPlaying("CENTRAL LIGHT ANIMATION") && active)
+    animator->PlayAnimation("CENTRAL LIGHT ANIMATION");
 
 }
 void CentralLightScript4::FixedComponentUpdate() {
