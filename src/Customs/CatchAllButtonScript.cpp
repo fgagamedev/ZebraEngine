99a2094956a1e95ee656cc9300c4144e2d880f41
amandabezerra <amandabs_contato@hotmail.com>
2017-09-18 00:04:10 -0300

    Second application of comments techniques.

--- src/Customs/CatchAllButtonScript.cpp
+++ src/Customs/CatchAllButtonScript.cpp
@@ -1,18 +1,36 @@
 #include "Customs/CatchAllButtonScript.hpp"
 
+/**
+    @file CatchAllButtonScript.cpp
+    @brief Manages the Catch All button functions.
+    @copyright LGPL. MIT License.
+*/
+
+/**
+    @brief Constructor of the class CatchAllButtonScript.
+    @param[in] GameObject *owner - Owns the component.
+*/
 CatchAllButtonScript::CatchAllButtonScript(GameObject *owner) : Script(owner) {}
 
+/**
+    @brief Starts the Catch All button.
+*/
 void CatchAllButtonScript::Start() {
-  m_uiButton = (UIButton *)GetOwner()->GetComponent("UIButton");
-  m_checkbox = SceneManager::GetInstance()->GetScene("Main")->GetGameObject(
-      "CatchAll_CB");
+    m_uiButton = (UIButton *)GetOwner()->GetComponent("UIButton");
+    m_checkbox = SceneManager::GetInstance()->GetScene("Main")->GetGameObject(
+                 "CatchAll_CB");
 }
 
+/**
+    @brief Updates the Quit button informations.
+*/
 void CatchAllButtonScript::ComponentUpdate() {
-  if (m_uiButton->IsClicked())
-    MenuController::GetInstance()->SelectGamemode(0x02);
-  if (MenuController::GetInstance()->GetGamemode() & 0x02)
-    m_checkbox->active = true;
-  else
-    m_checkbox->active = false;
+    if (m_uiButton->IsClicked()) {
+        MenuController::GetInstance()->SelectGamemode(0x02);
+    }
+    if (MenuController::GetInstance()->GetGamemode() & 0x02) {
+        m_checkbox->active = true;
+    } else {
+        m_checkbox->active = false;
+    }
 }
--- src/Customs/PlayButtonScript.cpp
+++ src/Customs/PlayButtonScript.cpp
@@ -1,31 +1,44 @@
 #include "Customs/PlayButtonScript.hpp"
 #include "Customs/AudioController.hpp"
 
+/**
+    @file PlayButtonScript.cpp
+    @brief Manages the Play button functions.
+    @copyright LGPL. MIT License.
+*/
+
+/**
+    @brief Constructor of the class PlayButtonScript.
+    @param[in] GameObject *owner - Owns the component.
+*/
 PlayButtonScript::PlayButtonScript(GameObject *owner) : Script(owner) {}
 
+/**
+    @brief Starts the Play button.
+*/
 void PlayButtonScript::Start() {
-  m_uiButton = (UIButton *)GetOwner()->GetComponent("UIButton");
+    m_uiButton = (UIButton *)GetOwner()->GetComponent("UIButton");
 }
 
+/**
+    @brief Updates the text, sound and menu of the Play button.
+*/
 void PlayButtonScript::ComponentUpdate() {
 
-	auto textButton = (UIText *)GetOwner()->GetComponent("UIText");
-  auto soundButton = (UISound *)GetOwner()->GetComponent("UISound");
-  auto menuSound = (UISound *)SceneManager::GetInstance()->GetCurrentScene()->GetGameObject("Music")->GetComponent("UISound");
-  if (m_uiButton->IsClicked()) {
-    soundButton->Play(0, -1);
-    menuSound->Stop();
-    AudioController::GetInstance()->PlayAudio("mainSound", -1);
-    SceneManager::GetInstance()->SetCurrentScene("Gameplay");
-  }
+    auto textButton = (UIText *)GetOwner()->GetComponent("UIText");
+    auto soundButton = (UISound *)GetOwner()->GetComponent("UISound");
+    auto menuSound = (UISound *)SceneManager::GetInstance()->GetCurrentScene()
+                                ->GetGameObject("Music")->GetComponent("UISound");
+    if (m_uiButton->IsClicked()) {
+        soundButton->Play(0, -1);
+        menuSound->Stop();
+        AudioController::GetInstance()->PlayAudio("mainSound", -1);
+        SceneManager::GetInstance()->SetCurrentScene("Gameplay");
+    }
 
-  if (m_uiButton->IsOver()) {
-    
-  	textButton->SetColor(160,160,160, 255);
-    
-
-  }else{
-  	
-  	textButton->SetColor(255,255,255, 255);
-  }
+    if (m_uiButton->IsOver()) {
+        textButton->SetColor(160,160,160, 255);
+    } else {
+        textButton->SetColor(255,255,255, 255);
+    }
 }
