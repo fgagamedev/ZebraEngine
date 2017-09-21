7dbe6b906ba01f4d7d0c52e5f9a16b44fb27f95c
Clarissa Borges <clarissalimab08@gmail.com>
2017-09-21 11:51:36 -0300

    Applying variable declaration commenting on MapScript class 	- There were only a few variables that needed commenting, but there were some indentations that were not as determinated on stylesheet. These indentations were corrected.

--- include/Customs/MapScript.hpp
+++ include/Customs/MapScript.hpp
@@ -13,52 +13,29 @@
 #include "Globals/EngineGlobals.hpp"
 
 class MapScript : public Script {
-//Right[45]=button lowerleft,Right[46]=button central,Right[47]=button centralupleft,right[48]=blueboss ->> reserved for buttons
-//right[49] = wall left down[48] = blue boss wall
-public:
-  MapScript(GameObject *owner);
-  std::string GetComponentName() override { return "MapScript"; };
-  void FixedComponentUpdate() override;
-  void Start() override;
-  int DetectWallCollision(GameObject* object);
+    public:
 
-  Vector leftWalls[100];
-  Vector leftWallsOriginal[100];
-  int leftWallsAmmount=0;
+    protected:
+        void ComponentUpdate() override;
 
-  Vector rightWalls[100];
-  int rightWallsAmmount=0;
-  Vector rightWallsOriginal[100];
-
-  Vector upWalls[100];
-  int upWallsAmmount=0;
-  Vector upWallsOriginal[100];
-
-  Vector downWalls[100];
-  Vector downWallsOriginal[100];
-  int downWallsAmmount=0;
-    bool activated=true;
-
-protected:
-  void ComponentUpdate() override;
-
-private:
-  void RenderWallsRects();
-  GameObject *nakedMan = nullptr;
-  GameObject *map = nullptr;
-  Vector *nakedManPosition = nullptr;
-  Vector *mapPosition = nullptr;
-  NakedManScript *script = nullptr;
-  InputSystem *input = nullptr;
-  char nakedManMovements;
-  int mapSize_x;
-  int mapSize_y;
-  int mapSpeed;
-  float proportion_x;
-  float proportion_y;
-  bool shake=false;
-  void CreateWalls();
-
+    private:
+        void RenderWallsRects();
+        GameObject *nakedMan = nullptr;
+        GameObject *map = nullptr;
+        Vector *nakedManPosition = nullptr;
+        Vector *mapPosition = nullptr;
+        NakedManScript *script = nullptr;
+        // player inputs
+        InputSystem *input = nullptr;
+        char nakedManMovements;
+        int mapSize_x;
+        int mapSize_y;
+        int mapSpeed;
+        float proportion_x;
+        float proportion_y;
+        // camera shaking
+        bool shake = false;
+        void CreateWalls();
 };
 
 #endif
