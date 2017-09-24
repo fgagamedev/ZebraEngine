2db66b216f5c35abc30a38307feecbd63263efe0
Clarissa Borges <clarissalimab08@gmail.com>
2017-09-24 11:42:15 -0300

    Applying variable declaration commenting on ForestActivatorScript class

--- src/Customs/ForestActivatorScript.cpp
+++ src/Customs/ForestActivatorScript.cpp
@@ -24,7 +24,8 @@ void ForestActivatorScript::Start() {
     GetOwner()->SetZoomProportion(Vector(0,0));
     auto map = SceneManager::GetInstance()->GetScene("Gameplay")->GetGameObject("Map");
     if (map) {
-        GetOwner()->SetZoomProportion(Vector(map->originalWidth/GetOwner()->originalWidth,map->originalHeight/GetOwner()->originalHeight));
+        GetOwner()->SetZoomProportion(Vector(map->originalWidth/GetOwner()->originalWidth,
+                                             map->originalHeight/GetOwner()->originalHeight));
     }
 }
 
@@ -33,7 +34,7 @@ void ForestActivatorScript::Start() {
 */
 void ForestActivatorScript::CreateAnimations() {
 
-    auto forestactivatorSprite = new Image("assets/forestactivator.png", 0, 0,832, 64);
+    auto forestactivatorSprite = new Image("assets/forestactivator.png", 0, 0, 832, 64);
     auto forestactivatorAnimation = new Animation(GetOwner(), forestactivatorSprite);
     for (int i = 0; i < 13; i++) {
         forestactivatorAnimation->AddFrame(new Frame(i * 64, 0, 64, 64));
@@ -53,10 +54,10 @@ void ForestActivatorScript::CreateAnimations() {
 */
 void ForestActivatorScript::ComponentUpdate() {
     // Starts animator
-    if (!animator->IsPlaying("FOREST ACTIVATOR ANIMATION") && activate==0 && runned==false) {
+    if (!animator->IsPlaying("FOREST ACTIVATOR ANIMATION") && activate == 0 && runned == false) {
         animator->PlayAnimation("FOREST ACTIVATOR ANIMATION");
-        activate=1;
-        runned=true;
+        activate = 1;
+        runned = true;
     }
 
     if (runned && !animator->IsPlaying("FOREST ACTIVATOR ANIMATION")) {
@@ -64,7 +65,10 @@ void ForestActivatorScript::ComponentUpdate() {
     }
 
     if (runned) {
-        auto script = (CentralLightScript3*)SceneManager::GetInstance()->GetCurrentScene()->GetGameObject("CENTRAL LIGHT 3")->GetComponent("CentralLightScript3");
+        auto script = (CentralLightScript3*)SceneManager::GetInstance()
+                                        ->GetCurrentScene()
+                                        ->GetGameObject("CENTRAL LIGHT 3")
+                                        ->GetComponent("CentralLightScript3");
         script->Activate();
     }
 }
