29222593384bad411a5d30e1bc9b4f468a9bf07e
Clarissa Borges <clarissalimab08@gmail.com>
2017-09-25 11:07:41 -0300

    Reinserting accidentally removed code on MapScript

--- include/Customs/MapScript.hpp
+++ include/Customs/MapScript.hpp
@@ -1,41 +1,65 @@
-#ifndef __MAP_SCRIPT__
-#define __MAP_SCRIPT__
+    #ifndef __MAP_SCRIPT__
+    #define __MAP_SCRIPT__
 
-#include "Engine/CameraSystem.hpp"
-#include "Engine/SceneManager.hpp"
+    #include "Engine/CameraSystem.hpp"
+    #include "Engine/SceneManager.hpp"
 
-#include "Components/Script.hpp"
-#include "Customs/NakedManScript.hpp"
-#include "Customs/ForestActivatorScript.hpp"
-#include "Customs/ForestActivatorScript2.hpp"
-#include "Customs/ForestActivatorScript3.hpp"
-#include "Customs/SnowActivatorScript.hpp"
-#include "Globals/EngineGlobals.hpp"
+    #include "Components/Script.hpp"
+    #include "Customs/NakedManScript.hpp"
+    #include "Customs/ForestActivatorScript.hpp"
+    #include "Customs/ForestActivatorScript2.hpp"
+    #include "Customs/ForestActivatorScript3.hpp"
+    #include "Customs/SnowActivatorScript.hpp"
+    #include "Globals/EngineGlobals.hpp"
 
-class MapScript : public Script {
-    public:
+    class MapScript : public Script {
+        public:
+            MapScript(GameObject *owner);
+            std::string GetComponentName() override {
+                return "MapScript";
+            };
+            void FixedComponentUpdate() override;
+            void Start() override;
+            int DetectWallCollision(GameObject* object);
 
-    protected:
-        void ComponentUpdate() override;
+            Vector leftWalls[100];
+            Vector leftWallsOriginal[100];
+            int leftWallsAmmount=0;
 
-    private:
-        void RenderWallsRects();
-        GameObject *nakedMan = nullptr;
-        GameObject *map = nullptr;
-        Vector *nakedManPosition = nullptr;
-        Vector *mapPosition = nullptr;
-        NakedManScript *script = nullptr;
-        // player inputs
-        InputSystem *input = nullptr;
-        char nakedManMovements;
-        int mapSize_x;
-        int mapSize_y;
-        int mapSpeed;
-        float proportion_x;
-        float proportion_y;
-        // camera shaking
-        bool shake = false;
-        void CreateWalls();
-};
+            Vector rightWalls[100];
+            int rightWallsAmmount=0;
+            Vector rightWallsOriginal[100];
 
-#endif
+            Vector upWalls[100];
+            int upWallsAmmount=0;
+            Vector upWallsOriginal[100];
+
+            Vector downWalls[100];
+            Vector downWallsOriginal[100];
+            int downWallsAmmount = 0;
+            bool activated = true;
+
+        protected:
+            void ComponentUpdate() override;
+
+        private:
+            void RenderWallsRects();
+            GameObject *nakedMan = nullptr;
+            GameObject *map = nullptr;
+            Vector *nakedManPosition = nullptr;
+            Vector *mapPosition = nullptr;
+            NakedManScript *script = nullptr;
+            // player inputs
+            InputSystem *input = nullptr;
+            char nakedManMovements;
+            int mapSize_x;
+            int mapSize_y;
+            int mapSpeed;
+            float proportion_x;
+            float proportion_y;
+            // camera shaking
+            bool shake = false;
+            void CreateWalls();
+    };
+
+    #endif
