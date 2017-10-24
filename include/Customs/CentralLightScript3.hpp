3c1cb0fe8bfb297cc61b9bdafeecf678c4582c6c
isaque alves de lima <isaquealvesdl@gmail.com>
2017-10-24 21:29:46 -0200

    Applyng técniques of comments and significance names

--- include/Customs/CentralLightScript3.hpp
+++ include/Customs/CentralLightScript3.hpp
@@ -15,24 +15,49 @@
 
 class CentralLightScript3 : public Script {
 
-public:
-  CentralLightScript3(GameObject *owner);
-  std::string GetComponentName() override { return "CentralLightScript3"; };
-  void FixedComponentUpdate() override;
-  void Start() override;
-  void Activate(){active = true;}
+    public:
 
-protected:
-  void ComponentUpdate() override;
-private:
-  bool active=false;
-  Timer time;
-  void CreateAnimations();
-  InputSystem *input = nullptr;
-   GameController* gamecontroller = nullptr;
-  Animator *animator = nullptr;
-  Vector *position = nullptr;
-  int play=0;
+        CentralLightScript3(GameObject *owner);
+
+        std::string GetComponentName() override {
+            return "CentralLightScript3";
+        };
+
+        void FixedComponentUpdate() override;
+
+        void Start() override;
+
+        void Activate(){
+            m_active = true;
+        }
+
+    private:
+
+        // Store if the script is m_active.
+        bool m_active = false;
+
+        // Store the time.
+        Timer m_time;
+
+        void CreateAnimations();
+
+        // Object for inputs in the central light script 3.
+        InputSystem *m_input = nullptr;
+
+        // Object for the inputs from the game controller.
+        GameController *m_gamecontroller = nullptr;
+
+        // Animator for the central light script 3.
+        Animator *m_animator = nullptr;
+
+        // Object that store positions in the game.
+        Vector *m_position = nullptr;
+
+        // Not used attribute.
+        int m_play = 0;
+
+    protected:
+        void ComponentUpdate() override;
 
 };
 #endif
--- src/Customs/CentralLightScript3.cpp
+++ src/Customs/CentralLightScript3.cpp
@@ -1,40 +1,78 @@
+/**
+	@file CentralLightScript3.cpp
+	@brief Responsible for the light animation during the menu
+    screen in center os screen.
+	@copyright MIT License.
+*/
 #include "Customs/CentralLightScript3.hpp"
 #include "Globals/EngineGlobals.hpp"
 
-CentralLightScript3::CentralLightScript3(GameObject *owner) : Script(owner) {}
+/**
+    @brief Constructor for the classe CentralLightScript3.
+*/
+CentralLightScript3::CentralLightScript3(GameObject *owner) : Script(owner) {
+
+}
+
+/**
+    @brief Sets the initial definitions when starting the animation.
+*/
 void CentralLightScript3::Start() {
 
-  CreateAnimations();
-  position = GetOwner()->GetPosition();
-  animator = (Animator *)GetOwner()->GetComponent("Animator");
-  input = InputSystem::GetInstance();
-  gamecontroller = input->GetGameController(0);
-  GetOwner()->SetZoomProportion(Vector(0,0));
+    // Creates the animations and the animator for the script.
+    CreateAnimations();
+
+    m_position = GetOwner()->GetPosition();
+
+    m_animator = (Animator *)GetOwner()->GetComponent("Animator");
+
+    // Get the inputs.
+    m_input = InputSystem::GetInstance();
+
+    m_gamecontroller = m_input->GetGameController(0);
+
+    GetOwner()->SetZoomProportion(Vector(0,0));
+
     auto map = SceneManager::GetInstance()->GetScene("Gameplay")->GetGameObject("Map");
-    if(map) GetOwner()->SetZoomProportion(Vector(map->originalWidth/GetOwner()->originalWidth,map->originalHeight/GetOwner()->originalHeight));
+
+    if(map) GetOwner()->SetZoomProportion(Vector(map->originalWidth / 
+                                                 GetOwner()->originalWidth, 
+                                                 map->originalHeight / 
+                                                 GetOwner()->originalHeight));
 
 }
+
+/**
+    @brief Generates the animations on the screen.
+*/
 void CentralLightScript3::CreateAnimations(){
 
-  auto centrallightSprite = new Image("assets/centro3.png", 0, 0,832, 64);
-  auto centrallightAnimation = new Animation(GetOwner(), centrallightSprite);
-  centrallightAnimation->AddFrame(new Frame(0, 0, 64, 64));
+    // Create the animation.
+    auto centrallightSprite = new Image("assets/centro3.png", 0, 0,832, 64);
+    auto centrallightAnimation = new Animation(GetOwner(), centrallightSprite);
+    centrallightAnimation->AddFrame(new Frame(0, 0, 64, 64));
 
+    // Create the animator.
+    auto centrallightAnimator = new Animator(GetOwner());
 
-  auto centrallightAnimator = new Animator(GetOwner());
-  centrallightAnimation->SetFramesPerSecond(9);
-  centrallightAnimator->AddAnimation("CENTRAL LIGHT ANIMATION", centrallightAnimation);
+    // Number of frames to add per second.
+    centrallightAnimation->SetFramesPerSecond(9);
+    centrallightAnimator->AddAnimation("CENTRAL LIGHT ANIMATION", centrallightAnimation);
 
 
 }
 
-
+/**
+    @brief Handles with changes on the component.
+*/
 void CentralLightScript3::ComponentUpdate() {
 
-if(!animator->IsPlaying("CENTRAL LIGHT ANIMATION") && active)
-animator->PlayAnimation("CENTRAL LIGHT ANIMATION");
-
+    if(!m_animator->IsPlaying("CENTRAL LIGHT ANIMATION") && m_active){
+        m_animator->PlayAnimation("CENTRAL LIGHT ANIMATION");
+    }
 }
+
+
 void CentralLightScript3::FixedComponentUpdate() {
 
 }
