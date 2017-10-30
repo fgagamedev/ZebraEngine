41e13a9373069ca5e78fc15b0f80226a17a3318e
isaque alves de lima <isaquealvesdl@gmail.com>
2017-10-29 22:14:57 -0200

    Apply techniques in HitScript class

--- include/Customs/HitScript.hpp
+++ include/Customs/HitScript.hpp
@@ -1,13 +1,21 @@
+/**
+    @file HitScripr.hpp
+    @brief Responsible for organizing sound effects script.
+    @copyright MIT License.
+*/
+
 #ifndef __HIT_SCRIPT__
 #define __HIT_SCRIPT__
 
 #include "Components/Animator.hpp"
-#include "Engine/InputSystem.hpp"
 #include "Components/Script.hpp"
+
+#include "Engine/InputSystem.hpp"
 #include "Engine/GameObject.hpp"
 #include "Engine/SceneManager.hpp"
 #include "Engine/CameraSystem.hpp"
 #include "Engine/Timer.hpp"
+
 #include "Math/Vector.hpp"
 
 #include <string>
@@ -15,27 +23,36 @@
 class HitScript : public Script {
 
 public:
-  HitScript(GameObject *owner);
-  std::string GetComponentName() override { return "HitScript"; };
-  void FixedComponentUpdate() override;
-  void Start() override;
 
+    HitScript(GameObject *owner);
 
-  bool hit = false;
-  int hitLevel = 0;
+    std::string GetComponentName() override {
+        return "HitScript";
+    };
 
+    void FixedComponentUpdate() override;
+
+    void Start() override;
+
+    bool hit = false;
+
+    int hitLevel = 0;
+
+private:
+
+    void CreateAnimations();
+
+    InputSystem *input = nullptr;
+
+    Animator *animator = nullptr;
+
+    Vector *position = nullptr;
+
+    Timer timerRecover;
 
 protected:
-  void ComponentUpdate() override;
-private:
-  void CreateAnimations();
-  InputSystem *input = nullptr;
-  Animator *animator = nullptr;
-  Vector *position = nullptr;
 
-  Timer timerRecover;
-
-
+    void ComponentUpdate() override;
 
 };
 
--- src/Customs/HitScript.cpp
+++ src/Customs/HitScript.cpp
@@ -1,76 +1,89 @@
+/**
+    @file HitScripr.cpp
+    @brief Responsible for organizing sound effects script.
+    @copyright MIT License.
+*/
+
 #include "Customs/HitScript.hpp"
+
 #include "Globals/EngineGlobals.hpp"
 
-HitScript::HitScript(GameObject *owner) : Script(owner) {}
+/**
+    @brief Constructor for the classe HitScript.
+*/
+HitScript::HitScript(GameObject *owner) : Script(owner) {
+
+}
+
+/**
+    @brief Sets the initial definitions when starting the sound effect.
+*/
 void HitScript::Start() {
 
-  CreateAnimations();
-  position = GetOwner()->GetPosition();
-  animator = (Animator *)GetOwner()->GetComponent("Animator");
-  input = InputSystem::GetInstance();
-  GetOwner()->SetZoomProportion(Vector(0,0));
+    // Creates the animations and the animator for the hit script.
+    CreateAnimations();
+
+
+    position = GetOwner()->GetPosition();
+
+    animator = (Animator *)GetOwner()->GetComponent("Animator");
+
+    // Get the inputs.
+    input = InputSystem::GetInstance();
+
+    GetOwner()->SetZoomProportion(Vector(0,0));
 
 }
+
+/**
+    @brief Generates the animations on the hit script.
+*/
 void HitScript::CreateAnimations(){
 
-  auto hitImage = new Image("assets/Sprites/Blood/blood4.png",0,0,1024, 1024);
+    auto hitImage = new Image("assets/Sprites/Blood/blood4.png",0,0,1024, 1024);
 
-  auto firstHitAnimation= new Animation(GetOwner(),hitImage );
-  firstHitAnimation->AddFrame(new Frame(0,0, 1024, 1024));
+    auto firstHitAnimation= new Animation(GetOwner(),hitImage);
 
-  // animator
-  auto hitAnimator = new Animator(GetOwner());
-  hitAnimator->AddAnimation("firstHitAnimation",firstHitAnimation);
+    firstHitAnimation->AddFrame(new Frame(0,0, 1024, 1024));
 
-  // hit 2
-
-  // hit 3
-
-  // hit 4
-
-  
-
+    // animator.
+    auto hitAnimator = new Animator(GetOwner());
+    hitAnimator->AddAnimation("firstHitAnimation",firstHitAnimation);
 
 }
 
-
+/**
+    @brief Handles with changes on the component.
+*/
 void HitScript::ComponentUpdate() {
 
     if(hit){
-
-      switch(hitLevel){
-
-        case 0: //No hit
-          //animator->StopAllAnimations();
-          hit = false;
-          break;
-        case 1://Hit 1
-          //hitAnimator->PlayAnimation("firstHitAnimation");
-            
-          break;
-        case 2:// Hit 2
-          break;
-        case 3:// Hit 3
-          break;
-
-      }
-
-
+        switch(hitLevel){
+            case 0: //No hit
+                hit = false;
+                break;
+            case 1://Hit 1          
+                break;
+            case 2:// Hit 2
+                break;
+            case 3:// Hit 3
+                break;
+        }
+    }else {
+        //nothing to do.
     }
 
-      
-    
-      
-
     if(input->GetKeyDown(INPUT_I)){
-      hit = true;
+        hit = true;
+    } else {
+        // nothing to do.
     }
 
 }
 
 void HitScript::FixedComponentUpdate() {
+
   position->m_x=0;
+
   position->m_y=0;
-
-
 }
